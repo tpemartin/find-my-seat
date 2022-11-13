88d95af7a34e2d08e3f1d244c1cf825dbadae88a
Martin老師 <tpemartin@gmail.com>
2022-11-13 21:38:04 +0800

    update

--- src/App.js
+++ src/App.js
@@ -4,45 +4,53 @@ import Plotly from 'plotly.js-dist'
 import {useFetch} from 'usehooks-ts'
 import config from './config.json'
 import { useEffect, useState } from 'react';
-import {Autocomplete, TextField} from '@mui/material'
-
-
+import {Autocomplete, TextField, Grid} from '@mui/material'
+import graphSelect from './graphSelect';
+window.Plotly = Plotly
+var graphDiv
 function App() {
+  const url = `${config.appscript}${config.spreadsheetId}.json`
+  console.log(url)
   var {data, isLoading, error} = useFetch(
-    config.appscript+'?spreadsheetId='+config.spreadsheetId
+    url
   )
   const [dd, setDd]=useState(data)
-  const [trace, setTrace]=useState(dd.data.length)
-  console.log(dd)
-  console.log(trace)
+  // const [trace, setTrace]=useState(dd?dd.data.length:null)
+  // console.log(dd)
+  // console.log(trace)
   useEffect(()=>{
-    const graphDiv = document.getElementById("seatingMap")
+    graphDiv = document.getElementById("seatingMap")
+    window.graphDiv = graphDiv
     if(data){
       setDd(data)
-      setTrace(data)
+      window.data=data.data
+      window.layout=data.layout
+      console.log(data.layout)
+      console.log(data.data)
+      // setTrace(data.data.length)
       Plotly.newPlot(graphDiv,data.data, data.layout)
       
     }
   })
+ 
+
   
   return (
     <div className="App">
-      <NameMenu dd={dd}/>
+      <Grid container spacing={2}>
+  <Grid item xs={3}>
+  <NameMenu dd={dd}/>
+  </Grid>
+  <Grid item xs={4}>
+   <div id={"seat"} />
+  </Grid>
+  <Grid item xs={12}>
+   
+  </Grid>
+</Grid>
+      
       <div id="seatingMap"></div>
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      
     </div>
   );
 }
@@ -52,13 +60,11 @@ function NameMenu({dd}){
   return  <Autocomplete
   disablePortal
   id="combo-box-demo"
-  onChange = {(event: any, newValue: string | null) => {console.log(event.target.innerText)}}
+  onChange = {(event: any, newValue: string | null) => {graphSelect(graphDiv, dd, event)}}
   options={options}
   sx={{ width: 300 }}
-  renderInput={(params) => <TextField {...params} label="Movie" />}
+  renderInput={(params) => <TextField {...params} label="Select Or Type your name" />}
 />
 }
-function graphSelect(event){
 
-}
 export default App;
--- src/graphSelect.js
+++ src/graphSelect.js
@@ -1,12 +1,26 @@
 import Plotly from 'plotly.js-dist'
 
-function graphSelect(graphDiv, data, event){
+export default async function graphSelect(graphDiv, data, event){
+  var seatHolder = document.getElementById("seat")
+  
+    console.log( graphDiv.data)
+    
     var student = data.assignedSeat[event.target.innerText]
-    var newTrace = Plotly.addTraces(
-      graphDiv, {
-        type: 'scatter', mode: 'markers',
-        x: [student.x], y: [student.y]
-      }
-    )
-    return newTrace
+    console.log(student)
+    seatHolder.innerText = student?`Row: ${student.y}  Column: ${student.x}`:""
+    var studentPlotData = student?{
+      type: 'scatter', mode: 'markers',
+      x: [student.x], y: [student.y], marker: {size: 30}
+    }:null
+    console.log(studentPlotData)
+    // Plotly.addTraces(graphDiv, studentPlotData)
+    if(studentPlotData && graphDiv.data.length>1){
+    Plotly.deleteTraces(graphDiv,1)
+      .then(
+          () => {Plotly.addTraces(graphDiv, studentPlotData)})
+    } else if(studentPlotData && graphDiv.data.length === 1){
+      Plotly.addTraces(graphDiv, studentPlotData)
+    }
+    console.log(graphDiv)
+    return graphDiv
 }
